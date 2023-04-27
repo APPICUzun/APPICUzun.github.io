0d7ddefea1a83d8100b7923f829ccc5dbe6ad48d
bioben <benjaminahnemail@gmail.com>
2023-04-27 18:26:00 -0400

    Updates

---
+++ precache-manifest.76dde5875b80dded7f69e4ee04b937be.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "134e224f8a37ae37e210",
+    "url": "./static/css/main.311ab1bf.chunk.css"
+  },
+  {
+    "revision": "134e224f8a37ae37e210",
+    "url": "./static/js/main.134e224f.chunk.js"
+  },
+  {
+    "revision": "e0dfba74bd3115d1a6d7",
+    "url": "./static/js/1.e0dfba74.chunk.js"
+  },
+  {
+    "revision": "538649d7a54a81dcabad",
+    "url": "./static/css/2.ad0fc062.chunk.css"
+  },
+  {
+    "revision": "538649d7a54a81dcabad",
+    "url": "./static/js/2.538649d7.chunk.js"
+  },
+  {
+    "revision": "27c1f6ee3a7109751a23",
+    "url": "./static/js/runtime~main.27c1f6ee.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "./static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "9e9fb8632a26187e53b4a1484a08cc13",
+    "url": "./index.html"
+  }
+];
